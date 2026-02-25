6cc5c58ae16413274dc883df001ec2c0876bab93
Mr.Shree <172600005+shriram7057@users.noreply.github.com>
2026-02-25 21:22:18 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Sort Integers by The Number of 1 Bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            int countA = __builtin_popcount(a);
+            int countB = __builtin_popcount(b);
+            
+            if (countA == countB)
+                return a < b;      // Secondary sort: ascending integer
+            
+            return countA < countB; // Primary sort: number of 1 bits
+        });
+        
+        return arr;
+    }
+};
